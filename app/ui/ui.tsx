56da003f4facc54c0b68062ad303c5470688f562
seong su kim <know.warehouse02@gmail.com>
2025-07-17 17:24:29 +0900

    ui 구축

--- app/ui/ui.tsx
+++ app/ui/ui.tsx
@@ -1,12 +1,25 @@
+'use client';
+
+import { Button, Input } from '@material-tailwind/react';
+import Todo from '../components/Todo';
+
 export default function UI() {
   return (
-    <>
-      <main>
-        <h1>TODO LIST</h1>
-        <div>
-          <input type="search" placeholder="할일을 적어주세요" />
-        </div>
-      </main>
-    </>
+    <div className="w-2/3 mx-auto flex flex-col items-center py-10 gap-2">
+      <h1 className="text-xl">TODO LIST</h1>
+
+      <Input
+        label="Search TODO"
+        placeholder="할일을 검색하세요"
+        icon={<i className="fas fa-search" />}
+      />
+
+      <Todo />
+
+      <Button>
+        <i className="fas fa-plus mr-2" />
+        ADD TODO
+      </Button>
+    </div>
   );
 }
