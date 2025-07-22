6171d68da52a8b5d9c05e35f0fa7e71c03d26944
seong su kim <know.warehouse02@gmail.com>
2025-07-22 13:58:45 +0900

    not found modify

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,15 +4,15 @@ import style from './404.module.css';
 export default function NotFound() {
   return (
     <main className={style.not__main}>
-      <h1 className={style.not__h1}>404 - Page Not Found :(</h1>
+      <h1 className={style.not__h1}>404 - Page Not Found</h1>
       <p className={style.not__text}>
-        Sorry, the page you are looking for does not exist.
+        죄송합니다. 존재하지 않는 페이지입니다.{' '}
       </p>
       <p className={style.not__text}>
-        You can go back to the
+        다음 버튼을 누르고 홈 화면으로 이동하세요
         <br />
         <button className={style.not__button}>
-          <Link href="/">home page</Link>
+          <Link href="/">홈 화면으로 이동</Link>
         </button>
       </p>
     </main>
