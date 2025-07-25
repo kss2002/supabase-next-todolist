5848ae493016433cd3c0396c0c1959e525054f3c
seong su kim <know.warehouse02@gmail.com>
2025-07-25 21:42:25 +0900

    not found modify

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
     <main className={style.not__main}>
       <h1 className={style.not__h1}>404 - Page Not Found</h1>
       <p className={style.not__text}>
-        죄송합니다. 존재하지 않는 페이지입니다.{' '}
+        죄송합니다. 존재하지 않는 페이지입니다.
       </p>
       <p className={style.not__text}>
         다음 버튼을 누르고 홈 화면으로 이동하세요
